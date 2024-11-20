84637942698487d706e058c4dfacc3f2a0891653
Nauris Linde <naurislinde@gmail.com>
2024-11-20 15:44:52 +0200

    Improved code coverage macro

--- code-coverage.cmake
+++ code-coverage.cmake
@@ -1,6 +1,6 @@
 function(enable_coverage project_name)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-        target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
-        target_link_libraries(${project_name} INTERFACE --coverage)
+        target_compile_options(${project_name} INTERFACE --coverage -O0 -g -fprofile-arcs -ftest-coverage -fno-exceptions)
+        target_link_libraries(${project_name} INTERFACE --coverage -lgcov)
     endif()
 endfunction()
